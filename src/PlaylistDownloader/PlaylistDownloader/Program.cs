824f7ee5b6723b2b08006e79d4ab0785732aaa8b
Chris Harvey <chrischarvey@outlook.com>
2021-02-15 14:44:04 -0500

    Initial commit of prototype page

---
+++ src/PlaylistDownloader/PlaylistDownloader/Program.cs
@@ -0,0 +1,36 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace PlaylistDownloader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+				.AddUserSecrets(Assembly.GetEntryAssembly())
+				.Build();
+
+			var service = new YouTubeService(new BaseClientService.Initializer() { ApiKey = configuration["YouTubeApiKey"] });
+
+			var playlistId = service.GetUploadPlaylist("CaseyNeistat");
+
+			var items = service.GetAllItems(playlistId);
+
+			File.WriteAllText($"{playlistId}.json", JsonConvert.SerializeObject(items.Select(i => new
+			{
+				title = i.Snippet.Title,
+				videoId = i.Snippet.ResourceId.VideoId,
+				publishedAt = i.Snippet.PublishedAt
+			})));
+
+			Console.WriteLine($"Downloaded {playlistId}");
+		}
+	}
+}
---
+++ src/PlaylistDownloader/PlaylistDownloader/YouTubeServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlaylistDownloader
+{
+	static class YouTubeServiceExtensions
+	{
+		public static string GetUploadPlaylist(this YouTubeService service, string username)
+		{
+			var request=service.Channels.List("contentDetails");
+			request.ForUsername = username;
+			return request.Execute().Items.Single().ContentDetails.RelatedPlaylists.Uploads;
+		}
+
+		public static IEnumerable<PlaylistItem> GetAllItems(this YouTubeService service, string playlistId)
+		{
+			string nextToken = null;
+			do
+			{
+				var request = service.PlaylistItems.List("snippet");
+				request.PlaylistId = playlistId;
+				request.MaxResults = 50;
+				request.PageToken = nextToken;
+
+				var response = request.Execute();
+				foreach (var item in response.Items)
+					yield return item;
+
+				nextToken = response.NextPageToken;
+			} while (nextToken != null);
+		}
+	}
+}
